add nice box drawing and fixed scaling to fit entire plot in preview window, also adds y-labels

rootprint.py
@@ -3,7 +3,7 @@
 import ROOT as root
 import numpy as np
 import sys
-from os import system, name
+from os import system, name, get_terminal_size
 from simple_term_menu import TerminalMenu
 
 def clear():
@@ -38,8 +38,8 @@ def getBlock(i: int):
 
 def rebin(bins: np.ndarray, n: int):
     temp = bins.copy()
-    temp = np.trim_zeros(temp)
     while len(temp) > n:
+        temp = np.trim_zeros(temp)
         if (len(temp) / 3) > (len(temp) / 2):
             div2 = True
         else:
@@ -62,12 +62,12 @@ def rebin(bins: np.ndarray, n: int):
                 third[i] += temp[3 * i + 2]
             temp = third
     diff = n - len(temp)
-    return np.pad(temp, (0, diff))
+    return np.pad(temp, (int(diff / 2), int(diff / 2) + 1))
 
 def pixelate(bins: np.ndarray, n: int):
     fullblocks = np.zeros_like(bins)
     remainder = np.zeros_like(bins)
-    scaled = np.interp(bins, (bins.min(), bins.max()), (0, 8 * n))
+    scaled = np.interp(bins, (0, bins.max()), (0, 8 * n))
     for i, b in enumerate(scaled):
         fullblocks[i] = int(b // 8)
         remainder[i] = int(b % 8)
@@ -76,12 +76,21 @@ def pixelate(bins: np.ndarray, n: int):
 
 def preview_hist(tkeyname: str):
     global tfile
-    X_SCALE = 180
-    Y_SCALE = 40
+    global preview
+    term_size = get_terminal_size()
+    if preview:
+        X_SCALE = term_size.columns - 9
+        Y_SCALE = int(term_size.lines * 0.75) - 5
+    else:
+        X_SCALE = term_size.columns - 6
+        Y_SCALE = term_size.lines - 5
     hist = tfile.Get(tkeyname)
     bincontent = np.zeros(hist.GetNbinsX())
     for i in range(hist.GetNbinsX()):
         bincontent[i] = int(hist.GetBinContent(i))
+    Y_MAX_STR = str(int(bincontent.max()))
+    Y_LABELS = [str(int(y)) for y in np.linspace(0, int(bincontent.max()), Y_SCALE)]
+    X_SCALE -= len(Y_MAX_STR)
     bincontent = rebin(bincontent, X_SCALE)
     fullblocks, remainder = pixelate(bincontent, Y_SCALE)
     grid = np.zeros((X_SCALE, Y_SCALE))
@@ -90,16 +99,29 @@ def preview_hist(tkeyname: str):
             grid[i][j] = 8
             if remainder[i]:
                 grid[i][j+1] = remainder[i]
-    line = ""
+    if preview:
+        line = "╔" + "═" * (len(Y_MAX_STR) + 2) + "╦" + "═" * (X_SCALE + 1) + "╗\n"
+    else:
+        line = " " + tkeyname + "\n"
+        line += "╔" + "═" * (len(Y_MAX_STR) + 2) + "╦" + "═" * (X_SCALE + 1) + "╗\n"
     for j in range(Y_SCALE):
+        if j == 0:
+            line += "║ " + Y_MAX_STR + " ╢"
+        elif j == Y_SCALE - 1:
+            line += "║" + " " * (len(Y_MAX_STR)) + "0 ╢"
+        else:
+            length_diff = len(Y_MAX_STR) - len(Y_LABELS[Y_SCALE - j - 1])
+            line += "║ " + " " * length_diff + f"{Y_LABELS[Y_SCALE - j - 1]}" + " ╢"
         for i in range(X_SCALE):
             line += getBlock(grid[i][Y_SCALE - j - 1])
-        line += "\n"
+        line += " ║\n"
+    line += "╚" + "═" * (len(Y_MAX_STR) + 2) + "╩" + "═" * (X_SCALE + 1) + "╝"
 
     return line
 
 clear()
 fpath = sys.argv[1]
+preview = False
 tfile = root.TFile.Open(fpath, "READ")
 if len(sys.argv) == 3:
     keylist = [key for key in list_keys(tfile)]
@@ -108,5 +130,6 @@ if len(sys.argv) == 3:
     else:
         print('Key not found, run without a second argument to list available keys!')
 else:
+    preview = True
     terminal_menu = TerminalMenu(list_keys(tfile), preview_command=preview_hist, preview_size=0.75)
     menu_entry_index = terminal_menu.show()